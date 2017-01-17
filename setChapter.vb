2fb52d354f0557a1070f57c62ea385c10fca55b3
Suwat <suwat.tac@mail.pbru.ac.th>
2017-01-17 14:40:58 +0700

    Create setChapter.vb

---
+++ setChapter.vb
@@ -0,0 +1,22 @@
+Sub setChapStyle()
+    Selection.HomeKey unit:=wdLine
+    Selection.Style = ActiveDocument.Styles("Heading 1")
+    Selection.MoveDown unit:=wdLine, Count:=1
+    Selection.Style = ActiveDocument.Styles("Heading 2")
+End Sub
+
+Sub setChapter()
+Selection.HomeKey unit:=wdStory
+
+    With Selection.Find
+    .Forward = True
+    .Wrap = wdFindStop
+    .Text = "º··Õè"
+    .Execute
+    Do While .Found
+         setChapStyle
+         .Execute
+    Loop
+    End With
+
+End Sub
